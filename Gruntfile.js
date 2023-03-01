6a42d7b388a4edfa1b5bdc4ea67708fe74273708
Nikita05 <nikita.codewing@gmail.com>
2023-03-01 21:26:57 +0545

    added file

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+    var pkgInfo = grunt.file.readJSON('package.json');
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        //CSS minified files
+        cssmin: {
+            minify: {
+                files: [{
+                    expand: true,
+                    dest: 'css/',
+                    ext: '.min.css'
+                }, ],
+                files: {
+                    'style.min.css': 'style.css' // 'destination': 'source'
+                }
+            },
+        },
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass']
+            }
+        },
+        sass: {
+            dist: {
+                files: {
+                    'style.css': 'sass/style.scss', // 'destination': 'source'
+                }
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+
+    grunt.registerTask('default', ['sass', 'watch', 'cssmin']);
+};
